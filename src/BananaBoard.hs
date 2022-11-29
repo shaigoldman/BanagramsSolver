8c532f641a975d7e6523fee3cc12784425f0fa57
Shai Goldman <shaitzvi@gmail.com>
2022-11-29 15:42:15 -0500

    bug fixes

--- src/BananaBoard.hs
+++ src/BananaBoard.hs
@@ -19,29 +19,39 @@ placeWord word p@(y, x) d om@(OMatrix og@(y0, x0) m)
     | d == V = placeWordVert word (y, x) sizedOM
     
     where 
-          pp@(pY, pX) = (y+y0-1, x+x0-1)
-          endP = if d == H then (pY, pX + length word) else (pY + length word, pX)
-          sizedOM = resizeTo endP (resizeTo pp om)
+          endP = if d == H then (y, x+length word-1) else (y+length word-1, x)
+          sizedOM = resizeTo endP (resizeTo p om)
 
           placeWordHrzntl :: String -> (Int, Int) -> OMatrix -> OMatrix
-          placeWordHrzntl [] _ mat = mat
-          placeWordHrzntl (w:ws) (y, x) (OMatrix og@(y0, x0) mat) =
-             placeWordHrzntl ws (y, x+1) $ OMatrix og $ setElem w (y+y0-1, x+x0-1) mat
+          placeWordHrzntl [] _ m = m
+          placeWordHrzntl (w:ws) p@(y, x) (OMatrix og@(y0, x0) m) =
+             placeWordHrzntl ws (y, x+1) $ OMatrix og $ setElem w (addO p og) m
 
           placeWordVert :: String -> (Int, Int) -> OMatrix -> OMatrix
-          placeWordVert [] _ mat = mat
-          placeWordVert (w:ws) (qy, qx) (OMatrix og@(y0, x0) mat) = 
-            placeWordVert ws (qy+1, qx) $ OMatrix og $ setElem w (qy+y0-1, qx+x0-1) mat
+          placeWordVert [] _ m = m
+          placeWordVert (w:ws) p@(y, x) (OMatrix og@(y0, x0) m) = 
+            placeWordVert ws (y+1, x) $ OMatrix og $ setElem w (addO p og) m
 
           resizeTo :: (Int, Int) -> OMatrix -> OMatrix
-          resizeTo p@(y, x) om@(OMatrix og@(y0, x0) m) 
-            | y < 1 = let yoff = 1 + abs y in
-                resizeTo (1, x) $ OMatrix (y0 + yoff, x0) $ empty yoff (ncols m) <-> m
-            | x < 1 = let xoff = 1 + abs x in 
-                resizeTo (y, 1) $ OMatrix (y0, x0 + xoff) $ empty (nrows m) xoff <|> m 
-            | y > nrows m = resizeTo p $ OMatrix og $ m <-> empty (y - nrows m) (ncols m)
-            | x > ncols m = resizeTo p $ OMatrix og $ m <|> empty (nrows m) (x - ncols m)
+          resizeTo p om@(OMatrix og@(y0, x0) m) 
+            | y < 1 = let yoff = 2 + abs y - y0 in
+                resizeTo (1, x) $ OMatrix (y0 + yoff, x0) 
+                    $ empty yoff (ncols m) <-> m
+            | x < 1 = let xoff = 2 + abs x - x0 in 
+                resizeTo (y, 1) $ OMatrix (y0, x0 + xoff) 
+                    $ empty (nrows m) xoff <|> m 
+            | y > nrows m = resizeTo p 
+                $ OMatrix og $ m <-> empty (y - nrows m) (ncols m)
+            | x > ncols m = resizeTo p 
+                $ OMatrix og $ m <|> empty (nrows m) (x-ncols m)
             | otherwise = om
+            where (y, x) = addO p og
+          
+          addO :: (Int, Int) -> (Int, Int) -> (Int, Int)
+          addO (y, x) (y0, x0) = (addO1 y y0, addO1 x x0)
+          addO1 c c0 = c+c0-1
+        
+        
 
 
 -- the board has the matrix and a list of words and positions
@@ -64,3 +74,11 @@ starting :: OMatrix
 starting = OMatrix (1,1) $ fromLists ["elevator"]
 
 next = placeWord "he" (0, 1) V starting
+
+{-
+word="12345"
+p@(y, x) = (0,1)
+d=V
+om@(OMatrix og@(y0, x0) m)=next
+placeWord word p d om
+-}
