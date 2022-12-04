8f9b9323c73e513d0497ec2fcb94a4c95b0b6f48
Shai Goldman <shaitzvi@gmail.com>
2022-12-03 22:14:40 -0500

    update hand on word play

--- src/WordChooser.hs
+++ src/WordChooser.hs
@@ -2,40 +2,50 @@ module WordChooser (
     
 ) where
 
+{-# LANGUAGE BlockArguments #-}
+
 import Data.List (group, sort, groupBy, sortBy, elemIndex, maximumBy)
-import Data.Maybe (fromJust, isNothing)
+import Data.Maybe (fromJust, isNothing, mapMaybe)
 import Data.HashMap.Strict (HashMap, fromList, member, update)
+import Control.Monad (unless)
 import BananaBoard
 
-type CharMap = HashMap Char Int
+type Hand = HashMap Char Int
+
+playTile :: Char -> Hand -> Hand
+playTile = update dec
+    where dec :: Int -> Maybe Int
+          dec 1 = Nothing
+          dec n = Just (n-1)
 
 splitDict :: [String] -> [[String]]
 splitDict dict = groupBy lengthEq $ sortBy lengthCmp dict
     where lengthCmp x y = length y `compare` length x
           lengthEq x y = length x == length y
 
-toHand :: String -> CharMap
+toHand :: String -> Hand
 toHand hand = fromList $ map (\s -> (head s, length s)) 
     $ (group . sort) hand
 
-canBuild :: String -> CharMap -> Bool
-canBuild [] _ = True
-canBuild (w:ws) hand
-    | null hand = False
-    | otherwise = member w hand 
-        && canBuild ws (update dec w hand)
-    where dec :: Int -> Maybe Int
-          dec 1 = Nothing
-          dec n = Just (n-1)
+buildWord :: String -> Hand -> Maybe Hand
+buildWord [] hand = Just hand
+buildWord (w:ws) hand
+    | null hand || not (member w hand) = Nothing
+    | otherwise = buildWord ws $ playTile w hand  
 
-buildables :: [String] -> CharMap -> [String]
-buildables dict hand = filter (`canBuild` hand) dict
-
-bestWord :: [String] -> Maybe String
-bestWord [] = Nothing
-bestWord dict = Just $ maximumBy scoreCmp dict
+buildWords :: [String] -> Hand -> [(String, Hand)]
+buildWords dict hand = mapMaybe bw_pair dict
     where 
-        scoreCmp x y = scoreWord x `compare` scoreWord y
+          bw word = buildWord word hand
+          bw_pair word = case bw word of
+            Nothing -> Nothing
+            Just hand -> Just (word, hand)
+
+bestWord :: [(String, Hand)] -> Maybe (String, Hand)
+bestWord [] = Nothing
+bestWord buildables = Just $ maximumBy scoreCmp buildables
+    where 
+        scoreCmp (x, _) (y, _) = scoreWord x `compare` scoreWord y
         scoreWord :: String -> Int
         scoreWord w = sum $ map scoreChar w
         
@@ -45,12 +55,15 @@ bestWord dict = Just $ maximumBy scoreCmp dict
         freqOrd = "esiarntolcdugpmhbyfvkwzxjq"
 
 
-playFirstWord :: CharMap -> [[String]] -> Maybe Board
+playFirstWord :: Hand -> [[String]] -> Maybe (Hand, Board)
 playFirstWord hand [] = Nothing
 playFirstWord hand (d:ds)
     | isNothing best = playFirstWord hand ds
-    | otherwise = Just $ singleton $ fromJust best
-    where best = bestWord $ buildables d hand
+    | otherwise = let (word, newhand) = fromJust best in
+        Just (newhand, singleton word)
+    where 
+          buildables = buildWords d hand
+          best = bestWord buildables
 
 main :: IO ()
 main = do
